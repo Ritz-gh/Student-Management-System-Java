73d584083b88af208b46b2200dcd7dacf0df5f77
Ritz-gh <ranjanritesh864@gmail.com>
2025-08-12 02:52:02 +0530

    Update DatabaseConnection.java

--- src/main/java/com/studentapp/DatabaseConnection.java
+++ src/main/java/com/studentapp/DatabaseConnection.java
@@ -9,7 +9,7 @@ public class DatabaseConnection {
 
     private static final String DB_URL = "jdbc:mysql://localhost:3307/studentdb?useSSL=false&serverTimezone=UTC";
     private static final String USER = "root";
-    private static final String PASSWORD = "Ritesh123"; 
+    private static final String PASSWORD = "123"; 
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(DB_URL, USER, PASSWORD);
